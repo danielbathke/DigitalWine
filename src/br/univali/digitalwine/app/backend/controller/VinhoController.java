3633221720ddfc6d96fb0c15bb1c6a142f9d226f
Daniel Santos Bathke <danielbathke@gmail.com>
2012-05-14 22:07:36 -0300

    Criando estrutura inicial.

---
+++ src/br/univali/digitalwine/app/backend/controller/VinhoController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univali.digitalwine.app.backend.controller;
+
+import br.univali.digitalwine.framework.controller.Controller;
+
+/**
+ *
+ * @author 115231
+ */
+public class VinhoController extends Controller {
+    
+}
